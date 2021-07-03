fe510fd2416f5d034da4a8990a1213e7ee19bcd6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-03 14:55:29 -0300

    init

---
+++ src/main/java/com/example/study_SpringConfig/StudySpringConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_SpringConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_SpringConfig/StudySpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_SpringConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
