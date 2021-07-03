cb98a9e465a4f9c1b320d5c1a0563e764a86680d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-07-03 15:51:36 -0300

    dbconfig

--- src/main/java/com/example/study_SpringConfig/Config/DBConfiguration.java
+++ src/main/java/com/example/study_SpringConfig/Config/DBConfiguration.java
@@ -5,10 +5,46 @@
  */
 package com.example.study_SpringConfig.Config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 /**
  *
  * @author Samuel
  */
-public class Config {
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@Getter
+@Setter
+public class DBConfiguration {
+    
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+    
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
+        System.out.println("DB Connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
+    
+    @Profile("prod")
+    @Bean
+    public String productionDatabaseConnection() {
+            System.out.println("DB Connection for PRODUCTION - MYSQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_PROD - Production instance";
+    }
+
     
 }
